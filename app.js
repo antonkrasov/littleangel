48f5552519d78badf6f0d0834bb6376fb714c2e5
Anton Krasov <anton.krasov@gmail.com>
2020-04-09 14:14:07 -0700

    connect to mongo

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 
-const PORT = config.get('port') || 5000
-app.listen(PORT, () => console.log(`App started at ${PORT}`))
+const start = async () => {
+    try {
+        const MONGOURI = config.get('mongoUrl')
+        await mongoose.connect(MONGOURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true 
+        })
+
+        const PORT = config.get('port') || 5000
+        app.listen(PORT, () => console.log(`App started at ${PORT}`))
+    } catch (e) {
+        console.error(`Couldn't start server, critical error`, e)
+        process.exit(1)
+    }
+}
+
+start()
+
+
+
+
+
