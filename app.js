5ed1f339d6d3be98463c29eee3153ba068d570a1
Anton Krasov <anton.krasov@gmail.com>
2020-04-09 13:49:01 -0700

    Base express and config setup

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('App!')
+const express = require('express')
+const config = require('config')
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+app.listen(PORT, () => console.log(`App started at ${PORT}`))
