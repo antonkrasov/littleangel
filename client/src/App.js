6023afe4116b2eeebc287e846c435ee36b5d5e95
Anton Krasov <anton.krasov@gmail.com>
2020-04-13 23:08:28 -0700

    Help buttons at the bottom of the page

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,29 @@ import SimpleMap from './components/SimpleMap'
 
 function App() {
   return (
-    <div>
+    <div className="root-app-container">
+
       <SimpleMap />
+
+      <div className="root-app-maps-overlay">
+        <div className="container h-100">
+
+          <div className="row align-items-end h-100">
+
+            <div className="col-md-4 offset-md-4 mb-2">
+
+              <div className="buttons-wrapper">
+                <button className="btn btn-primary btn-block btn-success">I can help</button>
+                <button className="btn btn-primary btn-block btn-warning">I need help</button>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
     </div>
   );
 }
